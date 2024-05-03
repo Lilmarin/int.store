8537dcb938e7ee2fef48daf6eb46b1d0d56e5211
AbelardoReyes <abelardoreyes256@gmail.com>
2024-05-03 15:44:13 -0600

    [FIX] Se ajusto carrusel

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -8,9 +8,9 @@ import chevron from "../Img/chevron-right.svg";
 import Carrusel from "./Error404/Carrusel.js";
 import background from "../Img/Backkground_Purple.png";
 import { NavLink } from "react-router-dom";
+
 const Error404 = () => {
   const images = [Circleint, Imagentelefono, PhoneTerminal];
-
   const [positionCarousel, setPositionCarousel] = useState(0);
   const moveToRight = () => {
     setPositionCarousel((current) => (current === 2 ? 0 : current + 1));
@@ -18,67 +18,66 @@ const Error404 = () => {
   const moveToLeft = () => {
     setPositionCarousel((current) => (current === 0 ? 2 : current - 1));
   };
+
   return (
     <>
       <div
         style={{
           fontFamily: "Red Hat Display",
           backgroundImage: `url(${background})`,
+          minHeight: "100vh", // Asegura que el contenedor tenga al menos el alto de la pantalla
         }}
-        className="relative w-full sm:flex sm:h-full sm:flex-col sm:px-[16px] md:h-screen md:px-[80px] lg:grid lg:grid-cols-2 lg:px-[100px]"
+        className="relative w-full overflow-auto sm:px-[16px] sm:py-[97px] md:px-[80px] lg:px-[100px] lg:py-[130px]"
       >
-        {/**Column 1*/}
-        <div className="w-full  sm:h-full md:h-[425px] lg:h-screen">
-          <section className="text-white sm:pt-[97px] lg:pt-[200px]">
-            <p className="sm:text-[12px] md:text-center md:text-[20px] lg:text-start">
-              {I18n.get("Error404Ups")}
-            </p>
-            <h1 className="mt-1 font-bold sm:text-[36px] sm:leading-[34px] md:text-center md:text-[48px] md:leading-[60px] lg:text-start lg:text-[72px] lg:leading-[90px]">
-              {I18n.get("ENotFound")}
-            </h1>
-            <h3 className="mt-4 font-light  sm:text-[12px] sm:leading-[18px] md:text-[20px] md:leading-[30px] lg:text-[24px] lg:leading-[32px]">
-              {I18n.get("EText")}
-            </h3>
-            <NavLink to="/registro_principal">
-              <button className="w-full rounded-[32px] bg-white text-[#2A2FAB] sm:mt-[24px] sm:h-[40px] sm:text-[14px] md:h-[44px] md:text-[16px] lg:mt-[32px] lg:h-[60px] lg:text-[18px]">
-                {I18n.get("GetDomain")}
-              </button>
-            </NavLink>
-          </section>
-        </div>
-        {/**Column 2*/}
-        <div className="relative flex w-full justify-center sm:mt-0 sm:h-[390px] md:h-[475px] lg:mt-0 lg:h-screen lg:items-center">
-          <Carrusel
-            positionCarousel={positionCarousel}
-            setPositionCarousel={setPositionCarousel}
-            moveToRight={moveToRight}
-          />
-          <div className="absolute z-50 flex flex-row gap-3 sm:bottom-14 md:bottom-3 lg:bottom-20">
-            {images.map((image, index) => (
-              <div
-                className={`h-3 w-3 rounded-full bg-white
-              ${index === positionCarousel ? "opacity-100" : "opacity-50"}`}
-              ></div>
-            ))}
+        <section className="flex sm:flex-col lg:h-[633px] lg:w-[1236px] lg:flex-row">
+          {/* Contenido */}
+          <div className="lg:w-[50%] lg:pt-[130px]">
+            <section className={`text-white`}>
+              <h4 className="sm:text-[12px] md:text-center md:text-[20px] lg:text-start">
+                {I18n.get("Error404Ups")}
+              </h4>
+              <h1 className="font-bold sm:text-[36px] sm:leading-[44px] md:text-center md:text-[48px] md:leading-[60px] lg:text-start lg:text-[70px] lg:leading-[90px]">
+                {I18n.get("ENotFound")}
+              </h1>
+              <p className="mt-2 font-light sm:text-[12px] sm:leading-[18px] md:text-[20px] md:leading-[30px] lg:text-[24px] lg:leading-[32px]">
+                {I18n.get("EText")}
+              </p>
+              <NavLink to="/registro_principal">
+                <button className="w-full rounded-[32px] bg-white text-[#2A2FAB] sm:mt-[24px] sm:h-[40px] sm:text-[14px] md:h-[44px] md:text-[16px] lg:mt-[32px] lg:h-[60px] lg:text-[18px]">
+                  {I18n.get("GetDomain")}
+                </button>
+              </NavLink>
+            </section>
           </div>
-          <div className="flex items-center justify-center">
-            <button
-              className="absolute z-20 flex h-9 w-9 rotate-180 items-center justify-center rounded-full bg-white sm:left-0 md:left-10 lg:left-0"
-              onClick={moveToLeft}
-            >
-              <img src={chevron} alt="Left" />
+          {/* Carrusel */}
+          <div className="relative sm:h-[360px] md:h-[470px] lg:h-auto lg:w-[50%]">
+            <Carrusel
+              positionCarousel={positionCarousel}
+              moveToRight={moveToRight}
+              moveToLeft={moveToLeft}
+            />
+            <button className="absolute top-1/2 z-10 flex h-9 w-9 items-center justify-center rounded-full bg-white sm:right-0 md:right-5 lg:right-0">
+              <img src={chevron} alt="chevron" onClick={moveToRight} />
             </button>
-            <button
-              onClick={moveToRight}
-              className="absolute z-20 flex h-9 w-9 items-center justify-center rounded-full bg-white sm:right-0 md:right-10 lg:right-0"
-            >
-              <img src={chevron} alt="Right" />
+            <button className="absolute top-1/2 z-10 flex h-9 w-9 rotate-180 items-center justify-center rounded-full bg-white sm:left-0 md:left-5 lg:left-0">
+              <img src={chevron} alt="chevron" onClick={moveToLeft} />
             </button>
+            {/*Indicador de imagen*/}
+            <section className="absolute bottom-0 left-0 right-0 flex justify-center gap-4 pb-4">
+              {images.map((image, index) => (
+                <div
+                  key={index}
+                  className={`h-3 w-3 rounded-full bg-white  ${
+                    index === positionCarousel ? "opacity-100" : "opacity-50"
+                  }`}
+                ></div>
+              ))}
+            </section>
           </div>
-        </div>
-        <p className="absolute left-0 right-0 mt-5 text-center text-white sm:bottom-5 lg:bottom-10">
+        </section>
+        <p className="absolute left-0 right-0 text-center text-[20px] text-white sm:bottom-[20px] md:bottom-[68px]">
           {I18n.get("IBrand")}
-        </p>{" "}
+        </p>
       </div>
     </>
   );
