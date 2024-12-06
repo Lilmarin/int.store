41abe96cc2610408f4df51d10955d3cb77b709ee
AbelardoReyes <abelardoreyes256@gmail.com>
2024-12-06 16:41:22 -0600

    feat: :bug: bug de formularios de ecommerce y terminal

--- src/app/intStore/Registro_Ecommerce.js
+++ src/app/intStore/Registro_Ecommerce.js
@@ -1,14 +1,4 @@
-import React, { useEffect } from "react";
-import "../../components/Style/Registro.scss";
-import {
-  Bottomtext,
-  DivColumnRegister,
-  Divcolumns2,
-  H1Home,
-  HText,
-  HText2,
-  PurpleSubtitle,
-} from "./StyledComponents";
+import React, { useEffect, useState } from "react";
 import Logoint from "../../Img/IconInt.png";
 import { I18n } from "aws-amplify/utils";
 import Form from "../../components/Form/Form";
@@ -16,83 +6,122 @@ import { scriptGoogle } from "../../lib/utils/scriptGoogle";
 
 const Registro = () => {
   const [loading, setLoading] = React.useState(true);
+  const [iframeStyle, setIframeStyle] = useState({
+    width: "500px",
+    height: "500px",
+  });
+
   useEffect(() => {
     scriptGoogle();
+    const handleResize = () => {
+      if (window.matchMedia("(max-width: 768px)").matches) {
+        setIframeStyle({
+          width: "100%", // Ajuste en móvil
+          height: "500px",
+        });
+      } else {
+        setIframeStyle({
+          width: "500px", // Ajuste en pantallas grandes
+          height: "500px",
+        });
+      }
+    };
+
+    // Llama la función al cargar y cada vez que cambia el tamaño de la ventana
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
+
   // Renderizar la salida del componente
   return (
-    <div className="registro-component">
-      <Divcolumns2
-        className="registro-container"
-        gap="0%"
-        columns="repeat(2, 1fr)"
-        columnsmobile="repeat(1, 1fr)"
-      >
-        <DivColumnRegister
-          width="77%"
-          alignitems="flex-start"
-          className="registro-left"
-        >
-          <img className="icon-int" alt="Logotipo Int.Store" src={Logoint} />
-          <H1Home lineheight="6vh" fontsize="6vh">
+    <section className="relative flex h-screen w-full font-bold sm:flex-col lg:flex-row">
+      <div className="relative flex w-full flex-col items-center justify-center bg-primary-40 text-white sm:h-[500px] sm:pt-14 lg:h-full lg:pt-0">
+        <div className="font-red-hat flex flex-col gap-2 sm:w-full sm:items-center sm:text-center lg:w-[556px] lg:items-start lg:text-left">
+          <img
+            className="icon-int h-14 w-14"
+            alt="Logotipo Int.Store"
+            src={Logoint}
+          />
+          <h1 className="sm:text-[25px] sm:leading-normal lg:text-[55px] lg:leading-[50px]">
             {I18n.get("REImproveyoursales")}
-          </H1Home>
-          <HText>{I18n.get("RETool")}</HText>
-          <div>
-            <Bottomtext
-              className="btn-text pb-2"
-              onClick={() => (window.location.href = "https://int.store")}
-            >
-              {I18n.get("IBrand")}
-            </Bottomtext>
-            <p
-              className="btn-text fontRedHat cursor-pointer text-[14px] text-white"
-              onClick={() =>
-                (window.location.href = "https://transactions.int.store")
-              }
-            >
-              {I18n.get("ILastTransactions")}
-            </p>
-          </div>
-        </DivColumnRegister>
-        <DivColumnRegister className="registro-right">
-          <div className="mt-[50px] flex h-full w-full flex-col items-center justify-center">
-            <PurpleSubtitle>{I18n.get("RPWaitList")}</PurpleSubtitle>
-            <HText2>{I18n.get("RPSingInNow")}</HText2>
-            {loading && (
-              <>
-                <Form />
-              </>
-            )}
-            <iframe
-              hidden={loading}
-              className="Iframe-form"
-              style={{
-                width: "80%",
-                height: "100%",
-              }}
-              src="https://link.superleads.mx/widget/form/JMoPiGk1Vvt9x1EnhPJa"
-              id="inline-JMoPiGk1Vvt9x1EnhPJa"
-              data-layout="{'id':'INLINE'}"
-              data-trigger-type="alwaysShow"
-              data-trigger-value=""
-              data-activation-type="alwaysActivated"
-              data-activation-value=""
-              data-deactivation-type="neverDeactivate"
-              data-deactivation-value=""
-              data-form-name="Lista de espera V2 _Ecommerce"
-              data-height="400"
-              data-layout-iframe-id="inline-JMoPiGk1Vvt9x1EnhPJa"
-              data-form-id="JMoPiGk1Vvt9x1EnhPJa"
-              title="Lista de espera V2 _Ecommerce"
-              onLoad={() => setLoading(false)}
-            ></iframe>
-
-            <script src="https://link.superleads.mx/js/form_embed.js"></script>
-          </div>
-        </DivColumnRegister>
-      </Divcolumns2>
-    </div>
+          </h1>
+          <p className="text-[20px] font-light">{I18n.get("RETool")}</p>
+        </div>
+        <footer className="absolute bottom-5 flex-col text-center sm:hidden lg:flex">
+          <p
+            className="cursor-pointer text-[12px] font-light"
+            onClick={() => (window.location.href = "https://int.store")}
+          >
+            {I18n.get("IBrand")}
+          </p>
+          <p
+            className="cursor-pointer text-[12px] font-light"
+            onClick={() =>
+              (window.location.href = "https://transactions.int.store")
+            }
+          >
+            {I18n.get("ILastTransactions")}
+          </p>
+        </footer>
+      </div>
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="flex flex-col items-center justify-center sm:w-full sm:px-4 lg:w-[556px] lg:px-[100px]">
+          <h1 className="cursor-pointer text-[30px] font-bold text-primary-50">
+            {I18n.get("RPWaitList")}
+          </h1>
+          <p className="text-[16px] font-light">{I18n.get("RPSingInNow")}</p>
+          {loading && (
+            <>
+              <Form />
+            </>
+          )}
+          <iframe
+            hidden={loading}
+            className="Iframe-form"
+            style={iframeStyle} // Estilo dinámico
+            src="https://link.superleads.mx/widget/form/JMoPiGk1Vvt9x1EnhPJa"
+            id="inline-JMoPiGk1Vvt9x1EnhPJa"
+            title="Lista de espera V2 _Ecommerce"
+            onLoad={() => setLoading(false)}
+          ></iframe>
+          {/* <iframe
+            hidden={loading}
+            className="Iframe-form"
+            style={{
+              width: "80%",
+              height: "100%",
+            }}
+            src="https://link.superleads.mx/widget/form/JMoPiGk1Vvt9x1EnhPJa"
+            id="inline-JMoPiGk1Vvt9x1EnhPJa"
+            data-layout="{'id':'INLINE'}"
+            data-trigger-type="alwaysShow"
+            data-trigger-value=""
+            data-activation-type="alwaysActivated"
+            data-activation-value=""
+            data-deactivation-type="neverDeactivate"
+            data-deactivation-value=""
+            data-form-name="Lista de espera V2 _Ecommerce"
+            data-height="400"
+            data-layout-iframe-id="inline-JMoPiGk1Vvt9x1EnhPJa"
+            data-form-id="JMoPiGk1Vvt9x1EnhPJa"
+            title="Lista de espera V2 _Ecommerce"
+            onLoad={() => setLoading(false)}
+          ></iframe> */}
+        </div>
+      </div>
+      <footer className="absolute bottom-2 w-full flex-col text-center text-primary-50 sm:flex lg:hidden">
+        <p
+          className="cursor-pointer text-[12px] font-light"
+          onClick={() => (window.location.href = "https://int.store")}
+        >
+          {I18n.get("IBrand")}
+        </p>
+      </footer>
+    </section>
   );
 };
 export default Registro;
--- src/app/intStore/Registro_Terminal.js
+++ src/app/intStore/Registro_Terminal.js
@@ -1,14 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "../../components/Style/Registro.scss";
-import {
-  Bottomtext,
-  DivColumnRegister,
-  Divcolumns2,
-  H1Home,
-  HText,
-  HText2,
-  PurpleSubtitle,
-} from "./StyledComponents";
 import Logoint from "../../Img/IconInt.png";
 import { I18n } from "aws-amplify/utils";
 import Form from "../../components/Form/Form";
@@ -16,83 +7,122 @@ import { scriptGoogle } from "../../lib/utils/scriptGoogle";
 
 const Registro = () => {
   const [loading, setLoading] = React.useState(true);
-  const language = localStorage.getItem("language") ?? "en";
+  const [iframeStyle, setIframeStyle] = useState({
+    width: "500px",
+    height: "500px",
+  });
+
   useEffect(() => {
     scriptGoogle();
-  }, []);
-  return (
-    <div className="registro-component">
-      <Divcolumns2
-        className="registro-container"
-        gap="0%"
-        columns="repeat(2, 1fr)"
-        columnsmobile="repeat(1, 1fr)"
-      >
-        <DivColumnRegister
-          width="77%"
-          alignitems="flex-start"
-          className="registro-left"
-        >
-          <img className="icon-int" alt="Logotipo Int.Store" src={Logoint} />
-          <H1Home lineheight="6vh" fontsize="6vh">
-            {I18n.get("RTSales")}
-          </H1Home>
-          <HText>{I18n.get("RTOutil")}</HText>
-          <div>
-            <Bottomtext
-              className="btn-text pb-2"
-              onClick={() => (window.location.href = "https://int.store")}
-            >
-              {I18n.get("IBrand")}
-            </Bottomtext>
-            <p
-              className="btn-text fontRedHat text-whitefontRedHat cursor-pointer text-[14px]"
-              onClick={() =>
-                (window.location.href = "https://transactions.int.store")
-              }
-            >
-              {I18n.get("ILastTransactions")}
-            </p>
-          </div>
-        </DivColumnRegister>
-        <DivColumnRegister className="registro-right">
-          <div className="mt-[50px] flex h-full w-full flex-col items-center justify-center">
-            <PurpleSubtitle>{I18n.get("RPWaitList")}</PurpleSubtitle>
-            <HText2>{I18n.get("RTSininnow")}</HText2>
+    const handleResize = () => {
+      if (window.matchMedia("(max-width: 768px)").matches) {
+        setIframeStyle({
+          width: "100%", // Ajuste en móvil
+          height: "500px",
+        });
+      } else {
+        setIframeStyle({
+          width: "500px", // Ajuste en pantallas grandes
+          height: "500px",
+        });
+      }
+    };
 
-            {loading && (
-              <>
-                <Form />
-              </>
-            )}
-            <iframe
-              hidden={loading}
-              className="Iframe-form"
-              style={{
-                width: "80%",
-                height: "100%",
-              }}
-              src="https://link.superleads.mx/widget/form/AX2ylBDNIejjpZYbFKGo"
-              id="inline-AX2ylBDNIejjpZYbFKGo"
-              data-layout="{'id':'INLINE'}"
-              data-trigger-type="alwaysShow"
-              data-trigger-value=""
-              data-activation-type="alwaysActivated"
-              data-activation-value=""
-              data-deactivation-type="neverDeactivate"
-              data-deactivation-value=""
-              data-form-name="Lista de espera V1"
-              data-height="400"
-              data-layout-iframe-id="inline-AX2ylBDNIejjpZYbFKGo"
-              data-form-id="AX2ylBDNIejjpZYbFKGo"
-              title="Lista de espera V1"
-              onLoad={() => setLoading(false)}
-            ></iframe>
-            <script src="https://link.superleads.mx/js/form_embed.js"></script>
-          </div>
-        </DivColumnRegister>
-      </Divcolumns2>
-    </div>
+    // Llama la función al cargar y cada vez que cambia el tamaño de la ventana
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Renderizar la salida del componente
+  return (
+    <section className="relative flex h-screen w-full font-bold sm:flex-col lg:flex-row">
+      <div className="relative flex w-full flex-col items-center justify-center bg-primary-40 text-white sm:h-[500px] sm:pt-14 lg:h-full lg:pt-0">
+        <div className="font-red-hat flex flex-col gap-2 sm:w-full sm:items-center sm:text-center lg:w-[556px] lg:items-start lg:text-left">
+          <img
+            className="icon-int h-14 w-14"
+            alt="Logotipo Int.Store"
+            src={Logoint}
+          />
+          <h1 className="sm:text-[25px] sm:leading-normal lg:text-[55px] lg:leading-[50px]">
+            {I18n.get("REImproveyoursales")}
+          </h1>
+          <p className="text-[20px] font-light">{I18n.get("RETool")}</p>
+        </div>
+        <footer className="absolute bottom-5 flex-col text-center sm:hidden lg:flex">
+          <p
+            className="cursor-pointer text-[12px] font-light"
+            onClick={() => (window.location.href = "https://int.store")}
+          >
+            {I18n.get("IBrand")}
+          </p>
+          <p
+            className="cursor-pointer text-[12px] font-light"
+            onClick={() =>
+              (window.location.href = "https://transactions.int.store")
+            }
+          >
+            {I18n.get("ILastTransactions")}
+          </p>
+        </footer>
+      </div>
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="flex flex-col items-center justify-center sm:w-full sm:px-4 lg:w-[556px] lg:px-[100px]">
+          <h1 className="cursor-pointer text-[30px] font-bold text-primary-50">
+            {I18n.get("RPWaitList")}
+          </h1>
+          <p className="text-[16px] font-light">{I18n.get("RPSingInNow")}</p>
+          {loading && (
+            <>
+              <Form />
+            </>
+          )}
+          <iframe
+            hidden={loading}
+            className="Iframe-form"
+            style={iframeStyle} // Estilo dinámico
+            src="https://link.superleads.mx/widget/form/JMoPiGk1Vvt9x1EnhPJa"
+            id="inline-JMoPiGk1Vvt9x1EnhPJa"
+            title="Lista de espera V2 _Ecommerce"
+            onLoad={() => setLoading(false)}
+          ></iframe>
+          {/* <iframe
+            hidden={loading}
+            className="Iframe-form"
+            style={{
+              width: "80%",
+              height: "100%",
+            }}
+            src="https://link.superleads.mx/widget/form/JMoPiGk1Vvt9x1EnhPJa"
+            id="inline-JMoPiGk1Vvt9x1EnhPJa"
+            data-layout="{'id':'INLINE'}"
+            data-trigger-type="alwaysShow"
+            data-trigger-value=""
+            data-activation-type="alwaysActivated"
+            data-activation-value=""
+            data-deactivation-type="neverDeactivate"
+            data-deactivation-value=""
+            data-form-name="Lista de espera V2 _Ecommerce"
+            data-height="400"
+            data-layout-iframe-id="inline-JMoPiGk1Vvt9x1EnhPJa"
+            data-form-id="JMoPiGk1Vvt9x1EnhPJa"
+            title="Lista de espera V2 _Ecommerce"
+            onLoad={() => setLoading(false)}
+          ></iframe> */}
+        </div>
+      </div>
+      <footer className="absolute bottom-2 w-full flex-col text-center text-primary-50 sm:flex lg:hidden">
+        <p
+          className="cursor-pointer text-[12px] font-light"
+          onClick={() => (window.location.href = "https://int.store")}
+        >
+          {I18n.get("IBrand")}
+        </p>
+      </footer>
+    </section>
   );
 };
 export default Registro;
